3c2e3a34451b2edaf788980ca2e4b8fa33bf4644
castle11118888-hash <castle11118888@gmail.com>
2026-05-01 23:39:56 +0800

    Update Firebase configuration for testing environment

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,13 @@
 // 注意：真正保護資料的是 Firestore Rules 與 Auth 權限，不是隱藏 apiKey。
 
 export const firebaseConfig = {
-   apiKey: "AIzaSyAbQf6eh1KSnZhDZeSqc4RbJDjgIpqNd-I",
-  authDomain: "handover-system-cd60a.firebaseapp.com",
-  projectId: "handover-system-cd60a",
-  storageBucket: "handover-system-cd60a.firebasestorage.app",
-  messagingSenderId: "950652925778",
-  appId: "1:950652925778:web:51303af3c25a6a2cda6c8b",
+  apiKey: "AIzaSyAehk39wLArVIdYOWief4haID9caGMTo",
+  authDomain: "handover-system-test.firebaseapp.com",
+  projectId: "handover-system-test",
+  storageBucket: "handover-system-test.firebasestorage.app",
+  messagingSenderId: "912253248594",
+  appId: "1:912253248594:web:5741ad86a391e125e1eb86b",
+  measurementId: "G-ZYS8BD6TNN"
 };
 
 export const firebaseEnabled = true; // 貼好設定後改成 true
